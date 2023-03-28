98a8015de2e3ada8466bd7c71f4544cf65c25e9b
Dario Rivera <fermius.us@gmail.com>
2023-03-28 19:34:31 -0400

    tests: add test to get prince for 2 years domain

--- tests/Feature/Domains/GetPricingForDomainTest.php
+++ tests/Feature/Domains/GetPricingForDomainTest.php
@@ -42,4 +42,36 @@ class GetPricingForDomainTest extends TestCaseWithMockAuthentication
         $this->assertSame(200, $response->getResponse()->getStatusCode());
         $this->assertSame($jsonResponse, $response->toArray());
     }
+
+    /**
+     * @test
+     */
+    public function getPricingForSpecifiedYearsDomain()
+    {
+        $domainName = $this->faker->domainName;
+
+        $jsonResponse = [
+            'purchasePrice' => 25.98,
+            'renewPrice' => 31.98,
+            'transferPrice' => 9.99,
+        ];
+
+        $this->builder
+            ->when()
+                ->methodIs('GET')
+                ->pathMatch('/v4\/domains\/' . self::DOMAIN_REGEX . ':getPricing/')
+            ->then()
+                ->statusCode(200)
+                ->json($jsonResponse);
+
+        $service = new NameComApi($this->baseUri);
+        $service->setCredentials($this->username, $this->password);
+        $service->withHandler(new HttpMock($this->builder));
+
+        $response = $service->getPricingForDomain($domainName, 2);
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertSame(200, $response->getResponse()->getStatusCode());
+        $this->assertSame($jsonResponse, $response->toArray());
+    }
 }
